13990d15cda3baec5a65225135f57c483a9ea44b
vishad2 <31646315+vishad2@users.noreply.github.com>
2018-10-06 17:14:58 +0530

    Update QuickSort.cpp
    Quicksort implementation without using class

--- Sorting/Quick Sort/C++/QuickSort.cpp
+++ Sorting/Quick Sort/C++/QuickSort.cpp
@@ -4,78 +4,37 @@
 
 using namespace std;
 
-class QuickSort
+partition (arr[], low, high)
 {
-	/* This function takes last element as pivot,
-	places the pivot element at its correct
-	position in sorted array, and places all
-	smaller (smaller than pivot) to left of
-	pivot and all greater elements to right
-	of pivot */
-	public:
-	
-	int partition(int arr[], int low, int high)
-	{
-		int pivot = arr[high]; 
-		int i = (low-1); // index of smaller element
-		for (int j=low; j<high; j++)
-		{
-			// If current element is smaller than or
-			// equal to pivot
-			if (arr[j] <= pivot)
-			{
-				i++;
+    // pivot (Element to be placed at right position)
+    pivot = arr[high];  
+ 
+    i = (low - 1)  // Index of smaller element
 
-				// swap arr[i] and arr[j]
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-
-		// swap arr[i+1] and arr[high] (or pivot)
-		int temp = arr[i+1];
-		arr[i+1] = arr[high];
-		arr[high] = temp;
-
-		return i+1;
-	}
-
-
-	/* The main function that implements QuickSort()
-	arr[] --> Array to be sorted,
-	low --> Starting index,
-	high --> Ending index */
-	void sort(int arr[], int low, int high)
-	{
-		if (low < high)
-		{
-			/* pi is partitioning index, arr[pi] is 
-			now at right place */
-			int pi = partition(arr, low, high);
-
-			// Recursively sort elements before
-			// partition and after partition
-			sort(arr, low, pi-1);
-			sort(arr, pi+1, high);
-		}
-	}
-};
-
-	// Driver program
-int main()
+    for (j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap arr[i] and arr[j]
+        }
+    }
+    swap arr[i + 1] and arr[high])
+    return (i + 1)
+}
+quickSort(arr[], low, high)
 {
-		int arr[] = {10, 7, 8, 9, 1, 5};
-		int n = *(&arr + 1) - arr;
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        pi = partition(arr, low, high);
 
-		QuickSort ob;
-		ob.sort(arr, 0, n-1);
-
-		cout<<"sorted array: ";
-		for (int i=0; i<n; ++i)
-			cout<<arr[i]<<" ";
-		cout<<endl;
-		return 0;
+        quickSort(arr, low, pi - 1);  // Before pi
+        quickSort(arr, pi + 1, high); // After pi
+    }
 }
 
 /*This code is contributed by Vedant Vasishtha */
